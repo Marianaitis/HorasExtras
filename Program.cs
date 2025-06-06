b6f2a80cb4eac28f88c886fc456c38c573e4f25a
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-06-06 19:22:59 -0300

    atividade finalizada

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿{
+
+    {
+        Console.WriteLine("--- Horas Extras ---\n");
+        
+        TestarCalculo(12, 300, 0);
+        TestarCalculo(12, 300, 20);
+        TestarCalculo(24, 300, 0);
+        TestarCalculo(24, 300, 20);
+    }
+
+    static void TestarCalculo(double salarioHora, double horasTotais, double horasExtras)
+    {
+        Console.WriteLine($"Salário-hora {salarioHora:C}, {horasTotais} horas, " +
+                         $"{horasExtras} horas-extra, salário = {CalcularSalario(salarioHora, horasTotais, horasExtras):C}");
+    }
+
+    static double CalcularSalario(double salarioHora, double horasTotais, double horasExtras)
+    {
+        return (salarioHora * (horasTotais - horasExtras)) + (salarioHora * horasExtras * 1.4);
+    }
+}
+ 
